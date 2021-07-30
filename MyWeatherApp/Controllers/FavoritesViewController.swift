c85d47aaa9ab20e86fec19b3cc0c4ac5e6b2e314
Žofia Jošťáková <zjostak@gmail.com>
2021-07-30 14:54:55 +0200

    Commit

---
+++ MyWeatherApp/Controllers/FavoritesViewController.swift
@@ -0,0 +1,12 @@
+//
+//  FavoritesViewController.swift
+//  MyWeatherApp
+//
+//  Created by Zofka Jostakova on 26/07/2021.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+}
